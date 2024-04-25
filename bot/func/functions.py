c0588eb337496c73af861dbd100155f7cc9501ef
Marcelo Araujo <37603265+kreativitat@users.noreply.github.com>
2024-04-25 19:34:54 +0100

    Update functions.py

--- bot/func/functions.py
+++ bot/func/functions.py
@@ -6,11 +6,11 @@ from aiogram import types
 from asyncio import Lock
 from functools import wraps
 from dotenv import load_dotenv
+
 # --- Environment
 load_dotenv()
 # --- Environment Checker
 token = os.getenv("TOKEN")
-allowed_ids = list(map(int, os.getenv("USER_IDS", "").split(",")))
 admin_ids = list(map(int, os.getenv("ADMIN_IDS", "").split(",")))
 ollama_base_url = os.getenv("OLLAMA_BASE_URL")
 ollama_port = os.getenv("OLLAMA_PORT", "11434")
@@ -18,7 +18,6 @@ log_level_str = os.getenv("LOG_LEVEL", "INFO")
 
 # --- Other
 log_levels = list(logging._levelToName.values())
-# ['CRITICAL', 'ERROR', 'WARNING', 'INFO', 'DEBUG', 'NOTSET']
 
 # Set default level to be INFO
 if log_level_str not in log_levels:
@@ -28,9 +27,7 @@ else:
 
 logging.basicConfig(level=log_level)
 
-
 # Ollama API
-# Model List
 async def model_list():
     async with aiohttp.ClientSession() as session:
         url = f"http://{ollama_base_url}:{ollama_port}/api/tags"
@@ -40,12 +37,10 @@ async def model_list():
                 return data["models"]
             else:
                 return []
+
 async def generate(payload: dict, modelname: str, prompt: str):
-    # try:
     async with aiohttp.ClientSession() as session:
         url = f"http://{ollama_base_url}:{ollama_port}/api/chat"
-
-        # Stream from API
         async with session.post(url, json=payload) as response:
             async for chunk in response.content:
                 if chunk:
@@ -53,30 +48,7 @@ async def generate(payload: dict, modelname: str, prompt: str):
                     if decoded_chunk.strip():
                         yield json.loads(decoded_chunk)
 
-
 # Aiogram functions & wraps
-def perms_allowed(func):
-    @wraps(func)
-    async def wrapper(message: types.Message = None, query: types.CallbackQuery = None):
-        user_id = message.from_user.id if message else query.from_user.id
-        if user_id in admin_ids or user_id in allowed_ids:
-            if message:
-                return await func(message)
-            elif query:
-                return await func(query=query)
-        else:
-            if message:
-                if message and message.chat.type in ["supergroup", "group"]:
-                    return
-                await message.answer("Access Denied")
-            elif query:
-                if message and message.chat.type in ["supergroup", "group"]:
-                    return
-                await query.answer("Access Denied")
-
-    return wrapper
-
-
 def perms_admins(func):
     @wraps(func)
     async def wrapper(message: types.Message = None, query: types.CallbackQuery = None):
@@ -87,30 +59,23 @@ def perms_admins(func):
             elif query:
                 return await func(query=query)
         else:
+            response_text = "Access Denied: This command is reserved for administrators."
             if message:
-                if message and message.chat.type in ["supergroup", "group"]:
-                    return
-                await message.answer("Access Denied")
-                logging.info(
-                    f"[MSG] {message.from_user.first_name} {message.from_user.last_name}({message.from_user.id}) is not allowed to use this bot."
-                )
+                await message.answer(response_text)
             elif query:
-                if message and message.chat.type in ["supergroup", "group"]:
-                    return
-                await query.answer("Access Denied")
-                logging.info(
-                    f"[QUERY] {message.from_user.first_name} {message.from_user.last_name}({message.from_user.id}) is not allowed to use this bot."
-                )
-
+                await query.answer(response_text)
+            logging.info(f"Unauthorized access attempt by {user_id}")
     return wrapper
 
-
-def md_autofixer(text: str) -> str:
-    # In MarkdownV2, these characters must be escaped: _ * [ ] ( ) ~ ` > # + - = | { } . !
-    escape_chars = r"_[]()~>#+-=|{}.!"
-    # Use a backslash to escape special characters
-    return "".join("\\" + char if char in escape_chars else char for char in text)
-
+# Open access function decorator
+def open_access(func):
+    @wraps(func)
+    async def wrapper(message: types.Message = None, query: types.CallbackQuery = None):
+        if message:
+            return await func(message)
+        elif query:
+            return await func(query=query)
+    return wrapper
 
 # Context-Related
 class contextLock:
